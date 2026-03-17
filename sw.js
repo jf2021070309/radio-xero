86dfb20c9c58750acab66b458022171f9b53518b
jf2021070309 <jaimeelias.tacna.2016@gmail.com>
2026-03-17 07:06:36 -0500

    version 3

--- sw.js
+++ sw.js
@@ -1,1 +1,14 @@
-self.addEventListener("fetch", function(event) {});
+const CACHE_NAME = 'radio-xero-v2';
+
+self.addEventListener('install', (event) => {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(clients.claim());
+});
+
+self.addEventListener('fetch', (event) => {
+  // Solo manejamos el evento, no es necesario cachear todo para que sea instalable
+});
+
